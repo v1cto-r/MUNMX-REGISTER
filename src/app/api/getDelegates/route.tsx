13568210cb8f4f1b02283caee67b6a0ee5e4dd39
v1cto-r <delarosasanovic@gmail.com>
2024-04-15 01:02:57 -0600

    Revert "Whooops fix"
    This reverts commit f725b67204a60e968b164549a2e0615772b0a0e3.

--- src/app/api/getDelegates/route.tsx
+++ src/app/api/getDelegates/route.tsx
@@ -5,6 +5,7 @@ import { NextResponse } from "next/server";
 export const fetchCache = 'force-no-store';
 
 export async function GET(request: Request) {
+  await request.json();
   await connectMongoDB();
   const delegates = await Delegate.find({ registered: false });
 
